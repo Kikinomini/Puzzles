8631a2ecada235f5e39b52ecd3c4671a71f7936e
DaDobe <Tobias.Klemens94@web.de>
2016-05-31 16:52:04 +0200

    Attribute des Bicycles verlinkt

--- module/BikeStore/src/BikeStore/Model/Bicycle.php
+++ module/BikeStore/src/BikeStore/Model/Bicycle.php
@@ -2,6 +2,20 @@
 
 namespace BikeStore\Model;
 
+use BikeStore\Model\Equipment\Bell;
+use BikeStore\Model\Equipment\Brake;
+use BikeStore\Model\Equipment\Coat;
+use BikeStore\Model\Equipment\Dynamo;
+use BikeStore\Model\Equipment\FrontWheel;
+use BikeStore\Model\Equipment\GearShift;
+use BikeStore\Model\Equipment\Light;
+use BikeStore\Model\Equipment\MudGuard;
+use BikeStore\Model\Equipment\PannierRack;
+use BikeStore\Model\Equipment\Pedal;
+use BikeStore\Model\Equipment\RearWheel;
+use BikeStore\Model\Equipment\Saddle;
+use BikeStore\Model\Equipment\SaddleBar;
+use BikeStore\Model\Equipment\Tube;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -19,17 +33,83 @@ class Bicycle extends Article
     /** @var  string */
     protected $colour;
 
-    /** @var   */
-    protected $wheeleSize;
-    protected $bikeSize;
-    protected $saddleId;
+    /** @var   Saddle
+     * @ORM\ManyToOne(targetEntity="BikeStore\Model\Equipment\Saddle")
+     * @ORM\JoinColumn(name="SaddleId", referencedColumnName="id")
+     */
+    protected $saddle;
+
+    /** @var  SaddleBar
+     * @ORM\ManyToOne(targetEntity="BikeStore\Model\Equipment\SaddleBar")
+     * @ORM\JoinColumn(name="SaddleBarId", referencedColumnName="id")
+     */
+    protected $saddleBar;
+
+    /** @var  Handlebars
+     * @ORM\ManyToOne(targetEntity="\BikeStore\Model\Equipment\Handlebars")
+     * @ORM\JoinColumn(name="HandlebarsId", referencedColumnName="id") */
+    protected $handlebars;
+
+    /** @var  Bell
+     * @ORM\ManyToOne(targetEntity="BikeStore\Model\Equipment\Bell")
+     * @ORM\JoinColumn(name="BellId", referencedColumnName="id")*/
+    protected $bell;
+
+    /** @var  Brake
+     * @ORM\ManyToOne(targetEntity="BikeStore\Model\Equipment\Brake")
+     * @ORM\JoinColumn(name="BrakeId", referencedColumnName="id")*/
+    protected $brake;
+
+    /** @var  Coat
+     * @ORM\ManyToOne(targetEntity="BikeStore\Model\Equipment\Coat")
+     * @ORM\JoinColumn(name="CoatId", referencedColumnName="id")*/
+    protected $coat;
+
+    /** @var  Dynamo
+     * @ORM\ManyToOne(targetEntity="BikeStore\Model\Equipment\Dynamo")
+     * @ORM\JoinColumn(name="DynamoId", referencedColumnName="id")*/
+    protected $dynamo;
+
+    /** @var  FrontWheel
+     * @ORM\ManyToOne(targetEntity="BikeStore\Model\Equipment\FrontWheel")
+     * @ORM\JoinColumn(name="FrontWheelId", referencedColumnName="id")*/
+    protected $frontWheel;
+
+    /** @var  GearShift
+     * @ORM\ManyToOne(targetEntity="BikeStore\Model\Equipment\GearShift")
+     * @ORM\JoinColumn(name="GearShiftId", referencedColumnName="id")*/
+    protected $gearShift;
+
+    /** @var  Light
+    @ORM\ManyToOne(targetEntity="BikeStore\Model\Equipment\Light")
+     * @ORM\JoinColumn(name="LightId", referencedColumnName="id")*/
+    protected $light;
+
+    /** @var  MudGuard
+     * @ORM\ManyToOne(targetEntity="BikeStore\Model\Equipment\MudGuard")
+     * @ORM\JoinColumn(name="MudGuardId", referencedColumnName="id",nullable=true)*/
+    protected $mudGuard;
+
+    /** @var  PannierRack
+     * @ORM\ManyToOne(targetEntity="BikeStore\Model\Equipment\PannierRack")
+     * @ORM\JoinColumn(name="PannierRackId", referencedColumnName="id")*/
     protected $pannierRack; //Boolean
-    protected $frontShock; //Boolean
-    protected $rearShock; //Boolean
-    protected $genderType; //int
-    protected $gearType;
-    protected $gears; //int
-    protected $bikeType;
+
+    /** @var  Pedal
+     * @ORM\ManyToOne(targetEntity="BikeStore\Model\Equipment\Pedal")
+     * @ORM\JoinColumn(name="PedalId", referencedColumnName="id")*/
+    protected $pedal;
+
+    /** @var  RearWheel
+     * @ORM\ManyToOne(targetEntity="BikeStore\Model\Equipment\RearWheel")
+     * @ORM\JoinColumn(name="RearWheelId", referencedColumnName="id")*/
+    protected $rearWheel;
+
+    /** @var  Tube
+     * @ORM\ManyToOne(targetEntity="BikeStore\Model\Equipment\Tube")
+     * @ORM\JoinColumn(name="TubeId", referencedColumnName="id")*/
+    protected $tube;
+
 
 
     public function __construct()
