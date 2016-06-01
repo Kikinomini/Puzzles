d2d1bb4873c620cd0cfa2745bafe5ca661082b3c
Ainias <silasg@web.de>
2016-06-01 23:47:06 +0200

    kurzesUpdate

--- module/BikeStore/src/BikeStore/Controller/SetUpController.php
+++ module/BikeStore/src/BikeStore/Controller/SetUpController.php
@@ -9,6 +9,9 @@
 namespace BikeStore\Controller;
 
 
+use Application\Model\Manager\RoleManager;
+use Application\Model\Resource;
+use Application\Model\Role;
 use BikeStore\Model\Bicycle;
 use BikeStore\Model\Equipment\Bell;
 use BikeStore\Model\Equipment\Brake;
@@ -33,6 +36,71 @@ use Zend\Mvc\Controller\AbstractActionController;
 class SetUpController extends AbstractActionController
 {
 	public function insertTestDataAction()
+	{
+		$this->insertDefaultRoleResourceData();
+		$this->insertStandardArticles();
+	}
+
+	private function insertDefaultRoleResourceData()
+	{
+		/** @var RoleManager $roleManager */
+		$roleManager = $this->getServiceLocator()->get("roleManager");
+
+		$roles = $roleManager->getAllEntities();
+
+		if (count($roles) == 0)
+		{
+			$guestRole = new Role();
+			$guestRole->setName("gast");
+			$guestRole->setBeschreibung("Kein aktives oder eingeloggtes Mitglied");
+
+			$userRole = new Role();
+			$userRole->setName("user");
+			$userRole->setBeschreibung("Normales Mitglied");
+
+			$adminRole = new Role();
+			$adminRole->setName("admin");
+			$adminRole->setBeschreibung("Admin, darf alles");
+
+			$adminRole->getParents()->add($userRole);
+			$userRole->getChildren()->add($adminRole);
+
+			$offlineResource = new Resource();
+			$offlineResource->setName("offline");
+			$offlineResource->setBeschreibung("alles, was ein nicht eingeloggter User sehen darf");
+
+			$onlineResource = new Resource();
+			$onlineResource->setName("online");
+			$onlineResource->setBeschreibung("alles, was ein eingeloggter User sehen darf");
+
+			$defaultResource = new Resource();
+			$defaultResource->setName("default");
+			$defaultResource->setBeschreibung("darf jeder");
+
+			$adminResource = new Resource();
+			$adminResource->setName("admin");
+			$adminResource->setBeschreibung("darf nur ein Admin");
+
+			$userRole->getResources()->add($onlineResource);
+			$userRole->getResources()->add($defaultResource);
+
+			$guestRole->getResources()->add($offlineResource);
+			$guestRole->getResources()->add($defaultResource);
+
+			$adminRole->getResources()->add($adminResource);
+
+			$roleManager->save($adminRole);
+			$roleManager->save($userRole);
+			$roleManager->save($guestRole);
+
+			echo "Default Roles & Resources eingefügt\n";
+		}
+		else
+		{
+			echo "Rollen existieren bereits - Überspringe das Einfügen neuer Rollen\n";
+		}
+	}
+	private function insertStandardArticles()
 	{
 		/** @var ArticleManager $articleManager */
 		$articleManager = $this->getServiceLocator()->get("BikeStore.articleManager");
@@ -107,5 +175,7 @@ class SetUpController extends AbstractActionController
 		$bicycle->setHandlebars($handlebars);
 
 		$articleManager->save($bicycle);
+
+		echo "insert Article-Test-Data";
 	}
 }
