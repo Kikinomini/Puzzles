dd92fe93444b1e767819ea61f947bb8260b45d03
Ainias <silasg@web.de>
2016-06-08 18:55:38 +0200

    - kurzes Update

--- public/js/FilterForm.js
+++ public/js/FilterForm.js
@@ -2,12 +2,16 @@ var FilterForm = {
 
 	form: null,
 
-	setForm:function(form)
+	setForm: function(form)
 	{
 		this.form = form;
 	},
 	sendForm: function()
 	{
 		$(this.form).submit();
+	},
+	setSide: function (sideNumber)
+	{
+		$(this.form).find("#page").val(sideNumber);
 	}
 };
