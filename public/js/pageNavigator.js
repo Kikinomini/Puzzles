7f8f37d8924431adbc599dc21590164ed7b5eefa
Ainias <silasg@web.de>
2016-06-29 00:43:23 +0200

    - kurze Bugfixes

--- public/js/pageNavigator.js
+++ public/js/pageNavigator.js
@@ -5,11 +5,12 @@
 var pageNavigator = {
 	maxPage: 0,
 	actualPage: 0,
-	createdDiv: "0",
+	createdDiv: null,
+	maxSeitenNebeneinander: 5,
 
 
 	createButtons:function(){
-		if (pageNavigator.createdDiv != '0'){
+		if (pageNavigator.createdDiv != null){
 			return pageNavigator.createdDiv;
 		}
 		var out = "";
@@ -19,36 +20,46 @@ var pageNavigator = {
 				out = out + pageNavigator.createOneButton(i);
 			}
 		}else{
-			if (pageNavigator.actualPage <= 3){
-				for(i = 1; i<=4; i++){
+			if (pageNavigator.actualPage <= this.maxSeitenNebeneinander){
+				for(i = 1; i<= this.maxSeitenNebeneinander+1; i++){
 					out = out + pageNavigator.createOneButton(i);
 				}
 				out = out + pageNavigator.createPointDiv() + pageNavigator.createOneButton(pageNavigator.maxPage);
 			}else{
-				if(pageNavigator.actualPage+3 >=pageNavigator.maxPage){
+				if(pageNavigator.actualPage+this.maxSeitenNebeneinander >=pageNavigator.maxPage){
 					out = pageNavigator.createOneButton(1) + pageNavigator.createPointDiv();
-					for (i = pageNavigator.maxPage - 3; i<= pageNavigator.maxPage; i++){
+					for (i = pageNavigator.maxPage - this.maxSeitenNebeneinander; i<= pageNavigator.maxPage; i++){
 						out = out + pageNavigator.createOneButton(i)
 					}
 				}
 				else{
 					out = pageNavigator.createOneButton(1)+pageNavigator.createPointDiv();
-					for(i = pageNavigator.actualPage-1; i <= pageNavigator.actualPage+1; i++){
+					for(i = pageNavigator.actualPage-2; i <= pageNavigator.actualPage+2; i++){
 						out = out + pageNavigator.createOneButton(i);
 					}
 					out = out + pageNavigator.createPointDiv() + pageNavigator.createOneButton(pageNavigator.maxPage);
 				}
 			}
 		}
-		pageNavigator.createdDiv = "<div class='completePageNavigator row'>Seitennavigation:" + out + "</div>";
+		pageNavigator.createdDiv = "<div class='completePageNavigator row'>Seite " + out + "</div>";
+		$(this.createdDiv).find(".pageNavigatorButton").click(function(){
+			console.log("Test");
+		});
 		return pageNavigator.createdDiv;
+	},
+
+	addOnClickListener: function()
+	{
+		$(".pageNavigatorButton").click(function(){
+			pageNavigator.buttonPressed($(this).attr("data-page"));
+		});
 	},
 
 	createOneButton:function(num){
 		if(num == pageNavigator.actualPage){
-			return " <" + num + "> ";
+			return " <div class = 'currentPage'>" + num + "</div>";
 		}
-		return "<button type='pageNavigatorButton' onclick='pageNavigator.buttonPressed("+num+")'> <b><"+ num +"> </b></button>";
+		return "<div class = 'pageNavigatorButton' data-page = '"+num+"'>"+ num +" </div>";
 	},
 	createPointDiv:function (){
 		return "..."
